f33b63c1639af95f0db1c893361000399370aae1
SNurali <snurali1986@gmail.com>
2026-05-26 19:44:20 +0500

    fix: Express 5 SPA fallback (use app.use instead of app.get('*'))

--- server.js
+++ server.js
@@ -689,7 +689,7 @@ app.use(express.static(distPath, {
 }));
 
 // SPA fallback: any non-API route serves index.html
-app.get('*', (_req, res) => {
+app.use((_req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
 
